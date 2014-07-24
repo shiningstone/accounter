c55116348f5d79f694947fa56cfd532f9adf2325
bo.jiang <bo.jiang@finisar.com>
2014-07-24 21:24:29 +0800

    1 add most resources 2 add main interface(empty)

---
+++ src/com/shiningstone/accounter/MainActivity.java
@@ -0,0 +1,19 @@
+
+package com.shiningstone.accounter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends Activity implements OnClickListener {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+	}
+	
+	@Override
+	public void onClick(View v) {
+	}
+}
--- src/com/shiningstone/accounter/SplashScreenActivity.java
+++ src/com/shiningstone/accounter/SplashScreenActivity.java
@@ -1,9 +1,8 @@
-/**
- * 
- */
+
 package com.shiningstone.accounter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 
@@ -16,6 +15,10 @@ public class SplashScreenActivity extends Activity {
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
+		Intent i=new Intent(SplashScreenActivity.this,MainActivity.class);
+		startActivity(i);
+		this.finish();
+
 		return super.onTouchEvent(event);
 	}
 }
