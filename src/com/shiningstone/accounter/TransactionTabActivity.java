919e92c8f48fac983ec3e84afe37748fb6b82eeb
bo.jiang <bo.jiang@finisar.com>
2014-07-31 19:10:59 +0800

    1 refactoring: view objects gathering; 2 add itemSelected interface for spinner; 3 delete redundant files

--- src/com/shiningstone/accounter/TransactionTabActivity.java
+++ src/com/shiningstone/accounter/TransactionTabActivity.java
@@ -13,6 +13,8 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -21,7 +23,7 @@ import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 
-public class TransactionTabActivity extends Activity implements OnCheckedChangeListener {
+public class TransactionTabActivity extends Activity implements OnCheckedChangeListener,OnItemSelectedListener {
 	final static int INCOME_MODE = 0;
 	final static int PAYOUT_MODE = 1;
 	final static int EDIT_MODE = 2;
@@ -37,13 +39,16 @@ public class TransactionTabActivity extends Activity implements OnCheckedChangeL
 		TITLES = new String[]{getString(R.string.edit_income),getString(R.string.edit_expense)};
 		Intent intent = getIntent();
 		mMode = intent.getIntExtra("mode", 1);
+
+		LoadResources();
 		
 		ShowDate();
-		ShowCategories();
-		ShowSubCategories();
-		ShowAccounts();
-		ShowItems();
-		ShowStores();
+		
+		SpinnerShow( CategorySpn, OptionsOfCategory );
+		SpinnerShow( SubCateSpn, OptionsOfSubCategory);
+		SpinnerShow( AccountSpn, OptionsOfAccount);
+		SpinnerShow( ItemSpn, OptionsOfItem);
+		SpinnerShow( StoreSpn, OptionsOfStore);
 		
 		RadioButton rb1 = (RadioButton) findViewById(R.id.payout_tab_rb);
 		rb1.setChecked(true);
@@ -63,68 +68,36 @@ public class TransactionTabActivity extends Activity implements OnCheckedChangeL
 	
 	@Override
 	public void onCheckedChanged(CompoundButton view, boolean isChecked) {
-		RadioButton rb1 = (RadioButton) findViewById(R.id.payout_tab_rb);
-		FrameLayout corporation_fl = (FrameLayout)findViewById(R.id.corporation_fl);
-		FrameLayout empty_fl = (FrameLayout)findViewById(R.id.empty_fl);
 
-		if(rb1.isChecked()) {
-			corporation_fl.setVisibility(View.VISIBLE);
-			empty_fl.setVisibility(View.GONE);
+		if(ExpenseBtn.isChecked()) {
+			StoreFrm.setVisibility(View.VISIBLE);
+			EmptyFrm.setVisibility(View.GONE);
 		} else {
-			corporation_fl.setVisibility(View.GONE);
-			empty_fl.setVisibility(View.VISIBLE);
+			StoreFrm.setVisibility(View.GONE);
+			EmptyFrm.setVisibility(View.VISIBLE);
 		}
 	}
 	
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+		if( view==CategorySpn.getSelectedView() ) {
+			
+		}
+	}
+	
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+	}
+
+	
 	/**********************************************************
-	 * 
+	 *    private method
 	 **********************************************************/
-	private void ShowCategories() {
-		Resources res = this.getResources();
-		String[]  options = res.getStringArray(R.array.TBL_EXPENDITURE_CATEGORY);
-		Spinner   spinner = (Spinner) findViewById(R.id.first_level_category_spn);
-
-		ListviewShow(spinner,options);
-	}
-
-	private void ShowSubCategories() {
-		Resources res = this.getResources();
-		String[]  options = res.getStringArray(R.array.TBL_EXPENDITURE_SUB_CATEGORY_1);
-		Spinner   spinner = (Spinner) findViewById(R.id.sub_category_spn);
-
-		ListviewShow(spinner,options);
-	}
-
-	private void ShowAccounts() {
-		Resources res = this.getResources();
-		String[]  options = res.getStringArray(R.array.TBL_ACCOUNT);
-		Spinner   spinner = (Spinner) findViewById(R.id.account_spn);
-
-		ListviewShow(spinner,options);
-	}
-
-	private void ShowItems() {
-		Resources res = this.getResources();
-		String[]  options = res.getStringArray(R.array.TBL_ITEM);
-		Spinner   spinner = (Spinner) findViewById(R.id.project_spn);
-
-		ListviewShow(spinner,options);
-	}
-
-	private void ShowStores() {
-		Resources res = this.getResources();
-		String[]  options = res.getStringArray(R.array.TBL_STORE);
-		Spinner   spinner = (Spinner) findViewById(R.id.corporation_spn);
-		
-		ListviewShow(spinner,options);
-	}
-	
 	private void ShowDate() {
-		Button trade_time_btn = (Button) findViewById(R.id.trade_time_btn);
-		trade_time_btn.setText(format(calendar.getTime()));
+		TradeDateBtn.setText(format(calendar.getTime()));
 	}
 	
-	private void ListviewShow(Spinner target,String[] options) {
+	private void SpinnerShow(Spinner target,String[] options) {
 		List<String> list = new ArrayList<String>();
 		for (int i=0;i<options.length;i++ ) {
 			list.add(options[i]);
@@ -139,5 +112,46 @@ public class TransactionTabActivity extends Activity implements OnCheckedChangeL
 	private String format(Date date){
 		SimpleDateFormat ymd = new SimpleDateFormat("yyyy-MM-dd");
 		return ymd.format(date); 
+	}
+
+	/**********************************************************
+	 *    resources control
+	 **********************************************************/
+	private Spinner CategorySpn = null;
+	private Spinner SubCateSpn = null;
+	private Spinner AccountSpn = null;
+	private Spinner ItemSpn = null;
+	private Spinner StoreSpn = null;
+	
+	private String[] OptionsOfCategory = null;
+	private String[] OptionsOfSubCategory = null;
+	private String[] OptionsOfAccount = null;
+	private String[] OptionsOfItem = null;
+	private String[] OptionsOfStore = null;
+	
+	private Button TradeDateBtn = null;
+	private RadioButton ExpenseBtn = null;
+	private FrameLayout StoreFrm = null;
+	private FrameLayout EmptyFrm = null;
+	
+	private void LoadResources() {
+		Resources res = this.getResources();
+
+		OptionsOfCategory = res.getStringArray(R.array.TBL_EXPENDITURE_CATEGORY);
+		OptionsOfSubCategory = res.getStringArray(R.array.TBL_EXPENDITURE_SUB_CATEGORY_1);
+		OptionsOfAccount = res.getStringArray(R.array.TBL_ACCOUNT);;
+		OptionsOfItem = res.getStringArray(R.array.TBL_ITEM);
+		OptionsOfStore = res.getStringArray(R.array.TBL_STORE);;
+		
+		CategorySpn = (Spinner) findViewById(R.id.first_level_category_spn);
+		SubCateSpn = (Spinner) findViewById(R.id.sub_category_spn);
+		AccountSpn = (Spinner) findViewById(R.id.account_spn);
+		ItemSpn = (Spinner) findViewById(R.id.project_spn);
+		StoreSpn = (Spinner) findViewById(R.id.corporation_spn);
+
+		TradeDateBtn = (Button) findViewById(R.id.trade_time_btn);
+		ExpenseBtn = (RadioButton) findViewById(R.id.payout_tab_rb);
+		StoreFrm = (FrameLayout)findViewById(R.id.corporation_fl);
+		EmptyFrm = (FrameLayout)findViewById(R.id.empty_fl);
 	}
 }
