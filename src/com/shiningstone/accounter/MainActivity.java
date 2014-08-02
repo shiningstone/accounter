49d26b62511d82944a88229beddb3e447a24c557
bo.jiang <bo.jiang@finisar.com>
2014-08-02 21:31:17 +0800

    add database description file (Is it ok to use class only to describe a data structure?)

--- src/com/shiningstone/accounter/MainActivity.java
+++ src/com/shiningstone/accounter/MainActivity.java
@@ -28,15 +28,10 @@ public class MainActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.main_activity);
 
 		SummaryShow();
-		
-		ListDayShow();
-		ListWeekShow();
-		ListMonthShow();
-
-		findViewById(R.id.add_expense_quickly_btn).setOnClickListener(this);
-		findViewById(R.id.today_row_rl).setOnClickListener(this);
-		findViewById(R.id.week_row_rl).setOnClickListener(this);
-		findViewById(R.id.month_row_rl).setOnClickListener(this);
+		DaySummaryShow();
+		WeekSummaryShow();
+		MonthSummaryShow();
+		StartListen();
 	}
 	
 	@Override
@@ -57,6 +52,58 @@ public class MainActivity extends Activity implements OnClickListener {
 		}
 	}
 	
+	/*********************************************************
+				Resources control
+	*********************************************************/
+	private void DaySummaryShow() {
+		TextShow( R.id.today_datestr_tv, mDate.mYear + "/" + mDate.mMonth + "/" + mDate.mDay );
+		TextShow( R.id.today_expense_amount_tv, "- ¥ " + 4 );
+		TextShow( R.id.today_income_amount_tv, "¥ " + 5 );
+	}
+	
+	private void WeekSummaryShow() {
+		Date Monday = mDate.WeekStart();
+		Date Sunday = mDate.WeekEnd();
+		
+		TextShow( R.id.week_datestr_tv, Transform(Monday,SHOW_MONTH_DAY) + "-" + Transform(Sunday,SHOW_MONTH_DAY) );
+		TextShow( R.id.week_expense_amount_tv, "- ¥ " + 6 );
+		TextShow( R.id.week_income_amount_tv, "¥ " + 7 );
+	}
+	
+	private void MonthSummaryShow() {
+		Date LastDay = mDate.MonthEnd();
+		
+		TextShow( R.id.month_datestr_tv, mDate.mMonth + "/01" + "-" + Transform(LastDay,SHOW_MONTH_DAY) );
+		TextShow( R.id.month_expense_amount_tv, "- ¥ " + 8 );
+		TextShow( R.id.month_income_amount_tv, "¥ " + 9 );
+	}
+
+	private void SummaryShow() {
+		TextShow( R.id.month_tv, mDate.mMonth );
+		TextShow( R.id.income_amount_tv, "¥ " + 1 );
+		TextShow( R.id.expense_amount_tv, "¥ " + 2 );
+		TextShow( R.id.budget_balance_amount_tv, "¥ " + 3 );
+	}
+
+	private void TextShow(int id,String value) {
+		((TextView)findViewById(id)).setText(value);
+	}
+	
+	private void StartListen() {
+		findViewById(R.id.add_expense_quickly_btn).setOnClickListener(this);
+		findViewById(R.id.today_row_rl).setOnClickListener(this);
+		findViewById(R.id.week_row_rl).setOnClickListener(this);
+		findViewById(R.id.month_row_rl).setOnClickListener(this);
+	}
+	
+	private String Transform(Date date,String format){
+		SimpleDateFormat ymd = new SimpleDateFormat(format);
+		return ymd.format(date); 
+	}
+
+	/*********************************************************
+				Interface to other activities
+	*********************************************************/
 	private void StartTransactionTabActivity()
 	{
 		Intent in = new Intent(MainActivity.this, TransactionTabActivity.class);
@@ -75,46 +122,9 @@ public class MainActivity extends Activity implements OnClickListener {
 		startActivity(intent);
 	}
 
-	private void TextShow(int id,String value) {
-		((TextView)findViewById(id)).setText(value);
-	}
-	
-	private void ListDayShow() {
-		TextShow( R.id.today_datestr_tv, mDate.mYear + "/" + mDate.mMonth + "/" + mDate.mDay );
-		TextShow( R.id.today_expense_amount_tv, "- ¥ " + 4 );
-		TextShow( R.id.today_income_amount_tv, "¥ " + 5 );
-	}
-	
-	private void ListWeekShow() {
-		Date Monday = mDate.WeekStart();
-		Date Sunday = mDate.WeekEnd();
-		
-		TextShow( R.id.week_datestr_tv, Transform(Monday,SHOW_MONTH_DAY) + "-" + Transform(Sunday,SHOW_MONTH_DAY) );
-		TextShow( R.id.week_expense_amount_tv, "- ¥ " + 6 );
-		TextShow( R.id.week_income_amount_tv, "¥ " + 7 );
-	}
-	
-	private void ListMonthShow() {
-		Date LastDay = mDate.MonthEnd();
-		
-		TextShow( R.id.month_datestr_tv, mDate.mMonth + "/01" + "-" + Transform(LastDay,SHOW_MONTH_DAY) );
-		TextShow( R.id.month_expense_amount_tv, "- ¥ " + 8 );
-		TextShow( R.id.month_income_amount_tv, "¥ " + 9 );
-	}
-
-	private void SummaryShow() {
-		TextShow( R.id.month_tv, mDate.mMonth );
-		TextShow( R.id.income_amount_tv, "¥ " + 1 );
-		TextShow( R.id.expense_amount_tv, "¥ " + 2 );
-		TextShow( R.id.budget_balance_amount_tv, "¥ " + 3 );
-	}
-	
-
-	private String Transform(Date date,String format){
-		SimpleDateFormat ymd = new SimpleDateFormat(format);
-		return ymd.format(date); 
-	}
-
+	/*********************************************************
+				SmartDate
+	*********************************************************/
 	public class SmartDate {
 		public String mYear;
 		public String mMonth;
---
+++ src/com/shiningstone/accounter/db/MyDbInfo.java
@@ -0,0 +1,147 @@
+
+package com.shiningstone.accounter.db;
+
+class Field {
+	public static final int ID = 0;
+	public static final int NAME = 1;
+	public static final int AMOUNT = 2;
+
+	public String Name;
+	public String Type;
+	
+	public Field(String name,String type) {
+		Name = name;
+		Type = type;
+	}
+};
+
+class Table {
+	public String Name;
+	public Field[] Fields;
+	private int idx;
+	
+	public Table(String name) {
+		Name = name;
+		idx = 0;
+		
+		AddField(Field.ID);
+	}
+	
+	public void AddField(String fname,String ftype) {
+		Fields[idx] = new Field(fname,ftype);
+		idx++;
+	}
+	
+	public void AddField(int type) {
+		switch( type ) {
+			case Field.ID:
+				AddField("ID","INTEGER PRIMARY KEY AUTOINCREMENT");
+				break;
+			case Field.NAME:
+				AddField("NAME","TEXT");
+				break;
+			case Field.AMOUNT:
+				AddField("AMOUNT","DOUBLE");
+				break;
+			default:
+				break;
+		}
+	}
+};
+
+public class MyDbInfo {
+	private static Table[] Tables = null;
+	private static String[] TableNames = null;
+	private static String[][] FieldNames = null;
+	private static String[][] FieldTypes = null;
+
+	public MyDbInfo() {
+		TablesInit();
+		
+		int tblIdx = 0;
+		for(; tblIdx<Tables.length; tblIdx++) {
+			TableNames[tblIdx] = Tables[tblIdx].Name;
+			
+			int fieldIdx = 0;
+			for(; fieldIdx<Tables[tblIdx].Fields.length; fieldIdx++) {
+				FieldNames[tblIdx][fieldIdx] = Tables[tblIdx].Fields[fieldIdx].Name;
+				FieldTypes[tblIdx][fieldIdx] = Tables[tblIdx].Fields[fieldIdx].Type;
+			}
+		}
+	}
+	
+	public static String[] getTableNames() {
+		return TableNames;
+	}
+	
+	public static String[][] getFieldNames() {
+		return FieldNames;
+	}
+	
+	public static String[][] getFieldTypes() {
+		return FieldTypes;
+	}
+	
+	private void TablesInit() {
+		Tables[0] = new Table("EXPENSE_CATEGORY");
+		Tables[0].AddField(Field.NAME);
+		Tables[0].AddField("BUDGET","DOUBLE");
+
+		Tables[1] = new Table("EXPENSE_SUB_CATEGORY");
+		Tables[1].AddField(Field.NAME);
+		Tables[1].AddField("PARENT_CATEGORY_ID","INTEGER");
+
+		Tables[2] = new Table("INCOME_CATEGORY");
+		Tables[2].AddField(Field.NAME);
+
+		Tables[3] = new Table("INCOME_SUB_CATEGORY");
+		Tables[3].AddField(Field.NAME);
+		Tables[3].AddField("PARENT_CATEGORY_ID","INTEGER");
+		
+		Tables[4] = new Table("ACCOUNT_TYPE");
+		Tables[4].AddField(Field.NAME);
+		Tables[4].AddField("POSTIVE","DOUBLE");
+		
+		Tables[5] = new Table("ACCOUNT_SUB_TYPE");
+		Tables[5].AddField(Field.NAME);
+		Tables[5].AddField("PARENT_CATEGORY_ID","INTEGER");
+		
+		Tables[6] = new Table("ACCOUNT");
+		Tables[6].AddField(Field.NAME);
+		Tables[6].AddField("TYPE_ID","INTEGER");
+		Tables[6].AddField("SUB_TYPE_ID","INTEGER");
+		Tables[6].AddField("ACCOUNT_BALANCE","DOUBLE");
+		
+		Tables[7] = new Table("STORE");
+		Tables[7].AddField(Field.NAME);
+		
+		Tables[8] = new Table("ITEM");
+		Tables[8].AddField(Field.NAME);
+		
+		Tables[9] = new Table("EXPENSE");
+		Tables[9].AddField(Field.AMOUNT);
+		Tables[9].AddField("EXPENDITURE_CATEGORY_ID","INTEGER");
+		Tables[9].AddField("EXPENDITURE_SUB_CATEGORY_ID","INTEGER");
+		Tables[9].AddField("ACCOUNT_ID","INTEGER");
+		Tables[9].AddField("STORE_ID","INTEGER");
+		Tables[9].AddField("ITEM_ID","INTEGER");
+		Tables[9].AddField("DATE","TEXT");
+		Tables[9].AddField("MEMO","TEXT");
+		
+		Tables[10] = new Table("INCOME");
+		Tables[10].AddField(Field.AMOUNT);
+		Tables[10].AddField("INCOME_CATEGORY_ID","INTEGER");
+		Tables[10].AddField("INCOME_SUB_CATEGORY_ID","INTEGER");
+		Tables[10].AddField("ACCOUNT_ID","INTEGER");
+		Tables[10].AddField("ITEM_ID","INTEGER");
+		Tables[10].AddField("DATE","TEXT");
+		Tables[10].AddField("MEMO","TEXT");
+		
+		Tables[11] = new Table("TRANSFER");
+		Tables[11].AddField(Field.AMOUNT);
+		Tables[11].AddField("ACCOUNT_ID","INTEGER");
+		Tables[11].AddField("ITEM_ID","INTEGER");
+		Tables[11].AddField("DATE","TEXT");
+		Tables[11].AddField("MEMO","TEXT");
+	}
+}
