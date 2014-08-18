4317e8aa29cab091550f4a178be3052c53a1f40d
bo.jiang <bo.jiang@finisar.com>
2014-08-18 22:19:24 +0800

    refactor tranfer activity

--- src/com/shiningstone/accounter/TransferActivity.java
+++ src/com/shiningstone/accounter/TransferActivity.java
@@ -31,154 +31,47 @@ public class TransferActivity extends Activity implements OnClickListener,OnItem
 	CommonData commondata = CommonData.getInstance();
 	private Calendar calendar = Calendar.getInstance();
 	private DatePickerDialog datePicker = null;
-	private EditText edit = null;
-	private Editable etext = null;
 	private AlertDialog dialog = null;
 	
-	private ArrayList<String>	transfer_account = new ArrayList<String>();
-	private Spinner transfer_out_account_spn = null;
-	private Spinner transfer_in_account_spn = null;
-	private EditText transfer_amount_out_et = null;
-	private Button transfer_info_tradetime_btn = null;
 	
-	private ArrayList<String>	transfer_item = new ArrayList<String>();
-	private Spinner transfer_info_project_spn = null;
-	
-	private Button transfer_info_memo_btn = null;
-	private Button transfer_btn = null;
 	private boolean isInitSpn = true;
 	private String strNum = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.transfer_activity);
 		
-		transfer_out_account_spn = (Spinner) findViewById(R.id.transfer_out_account_spn);
-//		transfer_out_account_spn.setp
-		transfer_out_account_spn.setOnItemSelectedListener(this);
+		LoadResources();
+		StartListen();
+		SpinnerInit();
 		
-		transfer_in_account_spn = (Spinner) findViewById(R.id.transfer_in_account_spn);
-		transfer_in_account_spn.setOnItemSelectedListener(this);
-		
-		transfer_amount_out_et = (EditText) findViewById(R.id.transfer_amount_out_et);
-		transfer_amount_out_et.addTextChangedListener(new TextWatcher() {
-			
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				// TODO Auto-generated method stub
-				etext = transfer_amount_out_et.getText(); 
+		date_btn.setText(format(calendar.getTime()));
+	}
 
-				if(s.toString().indexOf(".") > -1){
-					if (s.length() > 12) {
-						transfer_amount_out_et.setText(strNum);
-						Selection.setSelection(etext, etext.length());
-					}else if(s.toString().indexOf(".")<s.length()-3){
-						transfer_amount_out_et.setText(s.subSequence(0, s.toString().indexOf(".")+3));
-						Selection.setSelection(etext, etext.length());
-					}
-				}else{
-					if (s.length() > 9) {
-						transfer_amount_out_et.setText(strNum);
-						Selection.setSelection(etext, etext.length());
-					}
-				}
-			}
-			
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {
-				// TODO Auto-generated method stub
-				strNum = s.toString();
-			}
-			
-			@Override
-			public void afterTextChanged(Editable s) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		transfer_info_tradetime_btn = (Button) findViewById(R.id.transfer_info_tradetime_btn);
-		transfer_info_tradetime_btn.setOnClickListener(this);
-		transfer_info_tradetime_btn.setText(format(calendar.getTime()));
-		
-		transfer_info_project_spn = (Spinner) findViewById(R.id.transfer_info_project_spn);
-		
-		transfer_info_memo_btn = (Button) findViewById(R.id.transfer_info_memo_btn);
-		transfer_info_memo_btn.setOnClickListener(this);
-		
-		transfer_btn = (Button) findViewById(R.id.transfer_btn);
-		transfer_btn.setOnClickListener(this);
-		init();
-	}
-	
-	private void init(){
-		for (AccountData item : commondata.mAccount.values()) {
-			transfer_account.add(item.Name);
-		}
-		LoadingSpinnerInfo(transfer_out_account_spn,transfer_account);
-		LoadingSpinnerInfo(transfer_in_account_spn,transfer_account);
-		
-		
-		for (CommonData.TransferItem item : commondata.mTransfer.values()) {
-			transfer_item.add(item.name);
-		}
-		LoadingSpinnerInfo(transfer_info_project_spn,transfer_item);
-	}
-	
-	private void LoadingSpinnerInfo(Spinner sp,ArrayList<String> list){
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.simple_spinner_item_gravity_right, list);
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		sp.setAdapter(adapter);
-	}
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		if(v == transfer_info_tradetime_btn){
-			datePicker = new DatePickerDialog(this, mDateSetListenerSatrt,
-					calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-			datePicker.show();
+		if(v == date_btn){
+			ShowDateEditor();
 		}
-		if(v == transfer_info_memo_btn){
-			edit = new EditText(this);
-			edit.setLines(5);
-			edit.setText(transfer_info_memo_btn.getText());
-			dialog = new AlertDialog.Builder(this)
-			.setTitle(getString(R.string.dialog_memo_title))
-			.setView(edit)
-			.setPositiveButton(getString(R.string.dialog_memo_ok), new DialogInterface.OnClickListener() {
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					// TODO Auto-generated method stub
-					transfer_info_memo_btn.setText(edit.getText());
-				}
-			}).setNegativeButton(getString(R.string.dialog_memo_cancle), new DialogInterface.OnClickListener() {
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					// TODO Auto-generated method stub
-					
-				}
-			}).show();
+		if(v == memo_btn){
+			ShowMemoEditor();
 		}
-		if(v == transfer_btn){
-			save();
+		if(v == ok_btn){
+			Save();
 		}
 	}
 	
-	private void save(){
+	private void Save(){
 		double transfer_amount;
 		transfer = new TransferData();
-		if (TextUtils.isEmpty(transfer_amount_out_et.getText().toString())) {
+		if (TextUtils.isEmpty(amount_et.getText().toString())) {
 			Toast.makeText(getApplicationContext(), getString(R.string.transfer_amount_empty), 1).show();
 			return;
 		}
 		
 		try {
-			transfer_amount = Double.valueOf(transfer_amount_out_et.getText().toString()).doubleValue();
+			transfer_amount = Double.valueOf(amount_et.getText().toString()).doubleValue();
 		} catch (NumberFormatException e) {
 			Toast.makeText(getApplicationContext(), getString(R.string.transfer_amount_number), 1).show();
 			return;
@@ -189,14 +82,14 @@ public class TransferActivity extends Activity implements OnClickListener,OnItem
 		}
 		
 		transfer.Amount= (-transfer_amount);
-		transfer.Account = transfer_out_account_spn.getSelectedItemPosition()+1;
-		transfer.Item = transfer_info_project_spn.getSelectedItemPosition()+1;
-		transfer.Date = transfer_info_tradetime_btn.getText().toString();
-		transfer.Memo = transfer_info_memo_btn.getText().toString();
+		transfer.Account = source_spn.getSelectedItemPosition()+1;
+		transfer.Item = category_spn.getSelectedItemPosition()+1;
+		transfer.Date = date_btn.getText().toString();
+		transfer.Memo = memo_btn.getText().toString();
 		commondata.Add(transfer);
 		
 		transfer.Amount= transfer_amount;
-		transfer.Account = transfer_in_account_spn.getSelectedItemPosition()+1;
+		transfer.Account = dest_spn.getSelectedItemPosition()+1;
 		commondata.Add(transfer);
 		
 		Toast.makeText(getApplicationContext(), getString(R.string.transfer_ok), 0).show();
@@ -204,17 +97,82 @@ public class TransferActivity extends Activity implements OnClickListener,OnItem
         this.finish();  
 	}
 	
-	private DatePickerDialog.OnDateSetListener mDateSetListenerSatrt = new DatePickerDialog.OnDateSetListener() {
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+		if(view == source_spn.getSelectedView()){
+		}
+	}
 
-		public void onDateSet(DatePicker view, int year, int monthOfYear,
-				int dayOfMonth) {
-			calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-			calendar.set(Calendar.MONTH, monthOfYear);
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+	}
+	
+	/****************************************************
+					private methods
+	****************************************************/
+	private void SpinnerInit(){
+		ArrayList<String> transfer_account = new ArrayList<String>();
+		for (AccountData item : commondata.mAccount.values()) {
+			transfer_account.add(item.Name);
+		}
+		LoadSpinner(source_spn,transfer_account);
+		LoadSpinner(dest_spn,transfer_account);
+		
+		ArrayList<String> transfer_item = new ArrayList<String>();
+		for (CommonData.TransferItem item : commondata.mTransfer.values()) {
+			transfer_item.add(item.name);
+		}
+		LoadSpinner(category_spn,transfer_item);
+	}
+	
+	private void LoadSpinner( Spinner sp,ArrayList<String> list ){
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.simple_spinner_item_gravity_right, list);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		sp.setAdapter(adapter);
+	}
+	
+	private void ShowDateEditor() {
+		datePicker = new DatePickerDialog(this, mDateSetListenerStart,
+				calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+		datePicker.show();
+	}
+	
+	private DatePickerDialog.OnDateSetListener mDateSetListenerStart = new DatePickerDialog.OnDateSetListener() {
+		public void onDateSet(DatePicker view, int year, int month, int day) {
+			calendar.set(Calendar.DAY_OF_MONTH, day);
+			calendar.set(Calendar.MONTH, month);
 			calendar.set(Calendar.YEAR, year);
-			transfer_info_tradetime_btn.setText(format(calendar.getTime()));
+			
+			date_btn.setText(format(calendar.getTime()));
 		}
 	};
-	
+
+	private EditText edit = null;
+	private void ShowMemoEditor() {
+		edit = new EditText(this);/* why this variable cannot be declared in this function ??? */
+
+		edit.setLines(5);
+		edit.setText(memo_btn.getText());
+		
+		dialog = new AlertDialog.Builder(this)
+			.setTitle(getString(R.string.dialog_memo_title))
+			.setView(edit)
+			.setPositiveButton(getString(R.string.dialog_memo_ok), 
+								new DialogInterface.OnClickListener() {
+									@Override
+									public void onClick(DialogInterface dialog, int which) {
+										memo_btn.setText( edit.getText() );
+									}
+								})
+			.setNegativeButton(getString(R.string.dialog_memo_cancle), 
+								new DialogInterface.OnClickListener() {
+									@Override
+									public void onClick(DialogInterface dialog, int which) {
+									}
+								})
+			.show();
+	}
+
 	private String format(Date date){
 		String str = "";
 		SimpleDateFormat ymd = null;
@@ -223,23 +181,65 @@ public class TransferActivity extends Activity implements OnClickListener,OnItem
 		return str;
 	}
 
-	@Override
-	public void onItemSelected(AdapterView<?> parent, View view, int position,
-			long id) {
-		// TODO Auto-generated method stub
-		if(isInitSpn){
-			isInitSpn = false;
-			transfer_in_account_spn.setSelection(1);
-		}
-		
-		if(view == transfer_out_account_spn.getSelectedView()){
-			
-		}
+	/****************************************************
+					resources
+	****************************************************/
+	private Spinner source_spn = null;
+	private Spinner dest_spn = null;
+	private EditText amount_et = null;
+	private Button date_btn = null;
+	private Spinner category_spn = null;
+	private Button memo_btn = null;
+	private Button ok_btn = null;
+	
+	private void LoadResources() {
+		source_spn = (Spinner) findViewById(R.id.transfer_out_account_spn);
+		dest_spn = (Spinner) findViewById(R.id.transfer_in_account_spn);
+		amount_et = (EditText) findViewById(R.id.transfer_amount_out_et);
+		date_btn = (Button) findViewById(R.id.transfer_info_tradetime_btn);
+		category_spn = (Spinner) findViewById(R.id.transfer_info_project_spn);
+		memo_btn = (Button) findViewById(R.id.transfer_info_memo_btn);
+		ok_btn = (Button) findViewById(R.id.transfer_btn);
 	}
+	
+	private void StartListen() {
+		source_spn.setOnItemSelectedListener(this);
+		dest_spn.setOnItemSelectedListener(this);
+		amount_et.addTextChangedListener( AmountWatcher );
+		date_btn.setOnClickListener(this);
+		memo_btn.setOnClickListener(this);
+		ok_btn.setOnClickListener(this);
+	}
+	
+	TextWatcher AmountWatcher = new TextWatcher() {
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before, int count) {
+			Editable etext = amount_et.getText(); 
 
-	@Override
-	public void onNothingSelected(AdapterView<?> parent) {
-		// TODO Auto-generated method stub
+			if(s.toString().indexOf(".") > -1){
+				if (s.length() > 12) {
+					amount_et.setText(strNum);
+					Selection.setSelection(etext, etext.length());
+				}else if(s.toString().indexOf(".")<s.length()-3){
+					amount_et.setText(s.subSequence(0, s.toString().indexOf(".")+3));
+					Selection.setSelection(etext, etext.length());
+				}
+			}else{
+				if (s.length() > 9) {
+					amount_et.setText(strNum);
+					Selection.setSelection(etext, etext.length());
+				}
+			}
+		}
 		
-	}
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+			strNum = s.toString();
+		}
+		
+		@Override
+		public void afterTextChanged(Editable s) {
+		}
+	};
 }
